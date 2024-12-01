f304f961a1ad6341014a51172030176da7c72c1c
Vadim Garifullin <GarifullinVadim2631@gmail.com>
2024-12-02 03:27:51 +0500

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,26 @@ import math
 
 
 def area(r):
+    """
+    Calculate the area of a circle with radius r.
+    
+    Parameters:
+    r (float): The radius of the circle.
+    
+    Returns:
+    float: The area of the circle.
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
+    """
+    Calculate the perimeter (circumference) of a circle with radius r.
+    
+    Parameters:
+    r (float): The radius of the circle.
+    
+    Returns:
+    float: The perimeter (circumference) of the circle.
+    """
     return 2 * math.pi * r
-
