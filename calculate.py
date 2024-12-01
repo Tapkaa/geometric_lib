6af0b8171cffdc97739c4d1dc69989d134f34872
Vadim Garifullin <GarifullinVadim2631@gmail.com>
2024-12-02 03:39:27 +0500

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -5,12 +5,13 @@ import triangle
 figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
 sizes = {
-    'circle-perimeter': 1, 'circle-area': 1,
-    'square-perimeter': 1, 'square-area': 1,
-    'triangle-perimeter': 3, 'triangle-area': 3
+    'circle-perimeter': 1,
+    'circle-area': 1,
+    'square-perimeter': 1,
+    'square-area': 1,
+    'triangle-perimeter': 3,
+    'triangle-area': 3
 }
-
-
 def calc(fig, func, size):
     if fig not in figs:
         return "Invalid shape"
@@ -31,15 +32,11 @@ if __name__ == "__main__":
     func = ''
     fig = ''
     size = list()
-
     while fig not in figs:
         fig = input(f"Enter figure name, available are {figs}:\n")
-
     while func not in funcs:
         func = input(f"Enter function name, available are {funcs}:\n")
-
     while len(size) != sizes.get(f"{func}-{fig}", 1):
         size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-
     result = calc(fig, func, size)
     print(f'{func} of {fig} is {result}')
