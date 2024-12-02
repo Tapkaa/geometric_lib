1ef208255c72e5db391a96720353e40e9805e1d6
Vadim Garifullin <GarifullinVadim2631@gmail.com>
2024-12-02 21:45:40 +0500

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,5 +1,5 @@
 import pytest
-from geometric_lib.calculate import calc, InvalidShapeError, InvalidFunctionError, InvalidParametersError
+from calculate import calc, InvalidShapeError, InvalidFunctionError, InvalidParametersError
 
 # Тесты для корректных данных
 
