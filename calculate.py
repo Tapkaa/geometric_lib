2240d66d95dbb8539099629f400b5093f73eab57
Vadim Garifullin <GarifullinVadim2631@gmail.com>
2024-12-02 20:40:08 +0500

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,6 +1,7 @@
 import circle
 import square
 import triangle
+
 figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
 sizes = {
@@ -16,16 +17,10 @@ def calc(fig, func, size):
     if func not in funcs:
         return "Invalid function"
 
-    sizes = {
-    'circle-perimeter': 1, 'circle-area': 1,
-    'square-perimeter': 1, 'square-area': 1,
-    'triangle-perimeter': 3, 'triangle-area': 3
-}
+    key = f'{fig}-{func}'
 
-key = f'{fig}-{func}'
-
-if key in sizes and len(size) != sizes[key]:
-    return f"Invalid parameters for {fig}"
+    if key in sizes and len(size) != sizes[key]:
+        return f"Invalid parameters for {fig}"
 
     required_size = sizes.get(f'{fig}-{func}')
     if required_size is None:
@@ -49,7 +44,7 @@ if __name__ == "__main__":
         fig = input(f"Enter figure name, available are {figs}:\n")
     while func not in funcs:
         func = input(f"Enter function name, available are {funcs}:\n")
-    while len(size) != sizes.get(f"{func}-{fig}", 1):
+    while len(size) != sizes.get(f"{fig}-{func}", 1):
         size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
     result = calc(fig, func, size)
     print(f'{func} of {fig} is {result}')
