d8c685a40f0b69595f8b7d9119b4c2f92383a19f
Vadim Garifullin <GarifullinVadim2631@gmail.com>
2024-12-06 21:23:18 +0500

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,24 +1,39 @@
 import pytest
-from square import area, perimeter
+from math import pi
+from circle import area, perimeter
 
-def test_square_area():
-    # Arrange
-    a = 4
 
-    # Act
-    result = area(a)
+def test_area():
+    radius = 1
+    res = area(radius)
+    assert res == pi
 
-    # Assert
-    expected_result = a * a
-    assert result == expected_result
 
-def test_square_perimeter():
-    # Arrange
-    a = 4
+def test_perimeter():
+    radius = 1
+    res = perimeter(radius)
+    assert res == 2 * pi
 
-    # Act
-    result = perimeter(a)
 
-    # Assert
-    expected_result = 4 * a
-    assert result == expected_result
+def test_area_zero():
+    radius = 0
+    res = area(radius)
+    assert res == 0
+
+
+def test_perimeter_zero():
+    radius = 0
+    res = perimeter(radius)
+    assert res == 0
+
+
+def test_area_neg():
+    radius = -1
+    with pytest.raises(AssertionError):
+        area(radius)
+
+
+def test_perimeter_neg():
+    radius = -1
+    with pytest.raises(AssertionError):
+        perimeter(radius)
